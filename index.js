f84cf04e232f1dfff7441755f59c7cd10130e90f
NisooJadhav <nisoojadhav@gmail.com>
2022-06-20 16:56:48 +0530

    added code for random linear gradient in body bg

--- index.js
+++ index.js
@@ -64,3 +64,18 @@ function parseDay(date, day) {
     const parseDay = day.find((day, i) => i + 1 === dayNow ? day : false)
     return parseDay
 }
+
+//code for random linear gradient
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+function bgChange() {
+    let hexColor = '#';
+    let hexColor2 = '#';
+    for (let i = 0; i <= 5; i++) {
+        hexColor += hex[Math.floor(Math.random() * hex.length)]
+    }
+    for (let i = 0; i <= 5; i++) {
+        hexColor2 += hex[Math.floor(Math.random() * hex.length)]
+    }
+    document.body.style.background = "linear-gradient(to right," + hexColor + "," + hexColor2 + ")"
+}
+window.load = bgChange();
